17c5c4f3e7b43e8229d6d11014bb38e5a1ebd08c
Douglas Shanks <15614951+douglas-shanks@users.noreply.github.com>
2025-04-07 08:12:43 -0500

    Add 100 its data from hotlum.

---
+++ hotlum/benchmark_read/late/cray_late_100/submit_hotlum_read_late_nx_256_ny_256_nz_256_nodes_16.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#SBATCH --job-name=hotlum-cray-read
+#SBATCH --output=%x.o%j
+#SBATCH --time=06:00:00
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
+#SBATCH --exclusive
+
+# Set the number of threads to 1
+#   This prevents any threaded system libraries from automatically
+#   using threading.
+export OMP_NUM_THREADS=1
+export OMP_PLACES=cores
+
+# Set the eager limit
+# (see also https://docs.archer2.ac.uk/user-guide/tuning/#setting-the-eager-limit-on-archer2)
+export FI_OFI_RXM_SAR_LIMIT=64K
+
+#export FI_MR_CACHE_MONITOR=memhooks
+#export FI_CXI_RX_MATCH_MODE=software
+#export FI_CXI_REQ_BUF_SIZE=25165824
+
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+export MPLCONFIGDIR=$PWD
+
+if test "cray" = "gnu"; then
+    echo "Loading the GNU Compiler Collection (GCC)"
+    module load PrgEnv-gnu
+    module load cray-hdf5-parallel
+    module load cray-netcdf-hdf5parallel
+    module load cray-dsmml
+    module load cray-openshmemx
+    module load cpe/24.11
+    export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CRAY_NETCDF_HDF5PARALLEL_PREFIX/lib
+elif test "cray" = "cray"; then
+    echo "Loading the Cray Compiling Environment (CCE)"
+    module load PrgEnv-cray
+    module load cce
+    module load cray-mpich
+    module load cray-hdf5-parallel
+    module load cray-netcdf-hdf5parallel
+    module load cray-dsmml
+    module load cray-openshmemx
+
+    # load latest modules
+    module load -f cpe/24.11
+    export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CRAY_NETCDF_HDF5PARALLEL_PREFIX/lib
+fi
+
+echo "Running on $SLURM_N16 nodes with $SLURM_NTASKS tasks."
+
+module list
+
+bin_dir=/lus/bnchlu1/shanks/EPIC/310325/parcel-clustering/build_cce/bin
+PATH=${bin_dir}:$PATH
+EXE=${bin_dir}/benchmark_read
+export MPIR_CVAR_CH4_OFI_ENABLE_RMA=0
+#export SHMEM_SYMMETRIC_SIZE=1G
+
+#sbcast --compress=none ${bin_dir}/benchmark_read /tmp/benchmark_read
+for i in $(seq 1 5); do
+    srun --nodes=16 \
+        --ntasks=1024 \
+        --unbuffered \
+        --distribution=block:block \
+        --hint=nomultithread \
+        ${bin_dir}/benchmark_read \
+        --dirname /lus/bnchlu1/shanks/EPIC/data/rt-256x256x256/late-time \
+        --ncbasename epic_rt_256x256x256_late \
+        --niter 100 \
+        --offset 1 \
+        --nfiles 10 \
+        --size-factor 1.5 \
+        --csvfname "hotlum-cray-shmem-read-late-nx-256-ny-256-nz-256-nodes-16" \
+        --comm-type "shmem"
+    for g in "p2p" "rma"; do
+        srun --nodes=16 \
+            --ntasks=1024 \
+            --unbuffered \
+            --distribution=block:block \
+            --hint=nomultithread \
+            ${bin_dir}/benchmark_read \
+            --dirname /lus/bnchlu1/shanks/EPIC/data/rt-256x256x256/late-time \
+            --ncbasename epic_rt_256x256x256_late \
+            --niter 100 \
+            --offset 1 \
+            --nfiles 10 \
+            --size-factor 1.5 \
+            --csvfname "hotlum-cray-$g-read-late-nx-256-ny-256-nz-256-nodes-16" \
+            --comm-type "$g"
+
+        if test "true" = "true"; then
+            srun --nodes=16 \
+                --ntasks=1024 \
+                --unbuffered \
+                --distribution=block:block \
+                --hint=nomultithread \
+                ${bin_dir}/benchmark_read \
+                --dirname /lus/bnchlu1/shanks/EPIC/data/rt-256x256x256/late-time \
+                --ncbasename epic_rt_256x256x256_late \
+                --niter 100 \
+                --offset 1 \
+                --nfiles 10 \
+                --size-factor 1.5 \
+                --csvfname "hotlum-cray-$g-read-late-nx-256-ny-256-nz-256-nodes-16-subcomm" \
+                --comm-type "$g" \
+                --subcomm
+        fi
+    done
+done
